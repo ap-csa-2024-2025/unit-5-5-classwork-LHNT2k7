7b79ea9f1f760c9bc30fc3f938b5b3efb7586139
LHNT2k7 <ltri9573@gmail.com>
2024-12-11 10:15:29 -0800

    sdfgh

--- Dog.java
+++ Dog.java
@@ -1,4 +1,21 @@
 public class Dog
 {
+    private String breed;
+    private String color;
+    private double size;
 
+    public void setBreed(String iBreed)
+    {
+        breed = ibreed;
+    }
+
+    public void setColor(String iColor)
+    {
+        color = iColor;
+    }
+
+    public void setSize(double iSize)
+    {
+        size = iSize;
+    }
 }
